4b438f24e22875f2d81e951e5a8be7ef9bad764e
Luiz Cornelli <luizcornelli.dev@gmail.com>
2021-01-07 23:36:51 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import NavBar from './NavBar';
 import Home from './Home';
+import Routes from './Routes';
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <Home/>
+      <Routes/>
     </>
   );
 }
